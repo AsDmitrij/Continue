bdd2e68aa27640644bd32825ef1f483378f5312d
Dmitry Asonov <asdmitrij@mail.ru>
2021-04-29 09:11:05 +0300

    Add problem

---
+++ src/test/java/Tests.java
@@ -0,0 +1,23 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tests {
+    @Test
+    public void firstTest()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void secondTest()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void thirdTest()
+    {
+        Assert.assertTrue(true);
+    }
+
+}
